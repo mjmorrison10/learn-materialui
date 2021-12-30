cdfc83da8206d29c45836a3022f36407f582205b
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-12-30 13:59:13 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import { CssBaseline } from '@material-ui/core';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
